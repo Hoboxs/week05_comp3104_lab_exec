b533441b00e9ebb59b9afffc7541ee965e0300fa
Hoboxs <huboux.noah@gmail.com>
2020-10-05 20:36:16 -0400

    Adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.874728ce0b9cd7a6feee9587819d02e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f3c744fa0c43dd5668e",
+    "url": "/static/css/main.a4e92413.chunk.css"
+  },
+  {
+    "revision": "4f3c744fa0c43dd5668e",
+    "url": "/static/js/main.4f3c744f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "c225105cba0fd0fa8c5d5fb29160848d",
+    "url": "/index.html"
+  }
+];
